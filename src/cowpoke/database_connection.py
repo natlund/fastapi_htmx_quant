c6c5773914d1dd800c8d84b14426f1fe3145f835
Nat Lund <natjlund@gmail.com>
2025-02-25 21:52:40 +0000

    Move sqlite db file into directory 'databases'. Provide sqlite_url_for_alembic

--- src/cowpoke/database_connection.py
+++ src/cowpoke/database_connection.py
@@ -1,8 +1,17 @@
+import os.path
+
 from sqlmodel import create_engine
 
 
-sqlite_filename = "cowpoke.db"
-sqlite_url = f"sqlite:///{sqlite_filename}"
+# sqlite_filename = "cowpoke.db"
+sqlite_filename = "cowpoke.sqlite"
+database_directory = "databases"
+sqlite_filepath = os.path.join(os.path.pardir, database_directory, sqlite_filename)
+
+sqlite_url = f"sqlite:///{sqlite_filepath}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(url=sqlite_url, connect_args=connect_args)
+
+sqlite_filepath_for_alembic = os.path.join(database_directory, sqlite_filename)
+sqlite_url_for_alembic = f"sqlite:///{sqlite_filepath_for_alembic}"
