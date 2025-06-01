16314e70638faae2264f7302172d9b419cfab26b
Nat Lund <natjlund@gmail.com>
2025-06-01 14:17:06 +0100

    Bugfix: Change img alt to correct description

--- src/processing/statistical_arbitrage.py
+++ src/processing/statistical_arbitrage.py
@@ -17,10 +17,12 @@ import seaborn
 import matplotlib.pyplot as plt
 
 
+temp_folder = "../temp/"
+
 start = "2014-01-01"
 end = "2015-01-01"
 
-file_name = f"stock_prices_{start}_{end}.csv"
+file_name = temp_folder + f"stock_prices_{start}_{end}.csv"
 
 symbol_list = ["META", "AMZN", "AAPL", "NFLX", "GOOG"]
 
@@ -56,15 +58,17 @@ def find_cointegrated_pairs(df):
             pvalue_matrix[i, j] = pvalue
 
             if pvalue < 0.05:
-                cointegrated_pairs.append((keys[i], keys[j]))
+                cointegrated_pairs.append((keys[i], keys[j], pvalue))
+
+    cointegrated_pairs.sort(key=lambda x: x[2])
 
     return score_matrix, pvalue_matrix, cointegrated_pairs
 
 
 scores, pvalues, pairs = find_cointegrated_pairs(df)
 
-print(scores)
-print(pvalues)
+# print(scores)
+# print(pvalues)
 print(pairs)
 
 seaborn.heatmap(
@@ -74,7 +78,7 @@ seaborn.heatmap(
     cmap='RdYlGn_r',
     mask=(pvalues >= 0.10)
 )
-plt.savefig("heatmap.png")
+plt.savefig(temp_folder + "heatmap.png")
 plt.clf()
 
 # Make regression plot of Amazon and Google prices.
@@ -94,38 +98,113 @@ max_x = S1.max()
 x = np.arange(start=min_x, stop=max_x, step=0.1)
 y = b * x + b0
 plt.plot(x, y, 'red')
-plt.savefig("regression.png")
+plt.savefig(temp_folder + "regression.png")
+plt.clf()
 print(min_x, max_x)
 
+# spread = S2 - b * S1  # Difference between actual Google price and Google price estimated from regression line.
+estimated_S2 = b * S1 + b0
+spread = S2 - estimated_S2
+plt.plot(spread)
+plt.savefig(temp_folder + "spread.png")
+plt.clf()
+print("spread")
 
-# spread = S2 - b * S1
-#
-# print("spread")
-# print(spread)
-# print(results.params)
-#
-#
+
 # def zscore(series):
 #     return (series - series.mean()) / np.std(series)
-#
-#
+
+zscore_spread = (spread - spread.mean()) / np.std(spread)
+
 # zscore(spread).plot()
-#
-# plt.axhline(zscore(spread).mean(), color='black')
-# plt.axhline(1.0, color='red', linestyle='--')
-# plt.axhline(-1.0, color='green', linestyle='--')
-# plt.legend(['Spread z-score', 'Mean', '+1', '-1'])
-#
-#
-# # Create a DataFrame with the signal and position size in the pair.
-# trades = pd.concat([zscore(spread), S2 - b * S1], axis=1)
-# trades.columns = ["signal", "position"]
-#
-# # Add a long and short position at the z-score levels
-# trades["side"] = 0.0
-# trades.loc[trades.signal <= -1, "side"] = 1
-# trades.loc[trades.signal >= 1, "side"] = -1
-#
+
+zscore_spread.plot()
+
+plt.axhline(zscore_spread.mean(), color='black')
+plt.axhline(1.0, color='red', linestyle='--')
+plt.axhline(-1.0, color='green', linestyle='--')
+plt.legend(['Spread z-score', 'Mean', '+1', '-1'])
+plt.savefig(temp_folder + "zscore_spread.png")
+plt.clf()
+
+
+# Create a DataFrame with the signal and position size in the pair.
+position = S2 - b * S1
+trades = pd.concat([position, zscore_spread], axis=1)
+trades.columns = ["position", "signal"]
+
+# Add a long and short position at the z-score levels
+trades["side"] = 0.0
+trades.loc[trades.signal <= -1, "side"] = 1
+trades.loc[trades.signal >= 1, "side"] = -1
+
+# PyQuant's original calculation of returns makes no sense...
+# trades["returns"] = trades.position.pct_change() * trades.side
+# trades["cum_returns"] = trades.returns.cumsum()
+
+# Build a better trading strategy...
+# Want a signal to buy as soon as 'side' is 1, and sell as soon as 'side' is -1.
+
+
+def calculate_short_long_signal(df):
+
+    return_sign = 0
+
+    position_sign = []
+    for x in df.side:
+        if x == 1:
+            return_sign = 1
+        elif x == -1:
+            return_sign = -1
+
+        position_sign.append(return_sign)
+
+    df["return_sign"] = position_sign
+
+    return df
+
+
+trades = calculate_short_long_signal(trades)
+trades["position_diff"] = trades.position.diff()
+trades["returns"] = trades.position_diff * trades.return_sign.shift()  # Try shift() to flip sign the day after zscore goes to -1.
+trades["cum_returns"] = trades.returns.cumsum()
+
+first_position = trades.loc[trades.return_sign != 0].iloc[0]
+opening_position = first_position.position
+
+print("Opening position", opening_position)
+
+trades["traded_returns"] = trades.cum_returns + opening_position
+trades["fractional_return"] = (trades.cum_returns + opening_position) / opening_position
+
+# print(trades)
+trades.to_csv(temp_folder + "trades.csv")
+
+fig, ax1 = plt.subplots()
+ax1.plot(trades.position)
+ax1.plot(trades.traded_returns)
+plt.savefig(temp_folder + "traded_returns.png")
+plt.clf()
+
+fig, ax1 = plt.subplots()
+ax1.plot(trades.position)
+ax1.set_xlabel("Date")
+
+ax2 = ax1.twinx()
+ax2.plot(trades.fractional_return, color='red')
+# ax2.plot(trades.returns, color='red')
+# ax2.plot(trades.cum_returns, color='green')
+plt.savefig(temp_folder + "trading.svg")
+plt.clf()
+
 # returns = trades.position.pct_change() * trades.side
-# returns.cumsum().plot()
+# returns.plot()
+# plt.savefig(temp_folder + "returns.png")
+# plt.clf()
+# trades.position.plot()
+# plt.savefig(temp_folder + "position.png")
+# plt.clf()
 #
+# returns.cumsum().plot()
+# plt.savefig(temp_folder + "cumulative_returns.png")
+# plt.clf()
