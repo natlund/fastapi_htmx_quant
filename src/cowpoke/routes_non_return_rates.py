bca321976c21c6fccc51ac05fb8aa132fdb6197e
Nat Lund <natjlund@gmail.com>
2025-06-30 23:36:57 +0100

    Generate PDF of Mating Report. Add download link for it

--- src/cowpoke/routes_non_return_rates.py
+++ src/cowpoke/routes_non_return_rates.py
@@ -5,6 +5,7 @@ import shutil
 from fastapi import APIRouter, Request
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
+from weasyprint import CSS, HTML
 
 from src.cowpoke.non_return_rate import calculate_non_return_rate_results
 
@@ -12,9 +13,11 @@ from src.cowpoke.non_return_rate import calculate_non_return_rate_results
 templates = Jinja2Templates(directory="templates")
 template_dir = "cowpoke"
 
-cow_submission_graph_file_name = "temp/cowpoke/cow_submissions_chart.svg"
+cow_submissions_chart_file_name = "temp/cowpoke/cow_submissions_chart.svg"
 returns_bar_chart_file_name = "temp/cowpoke/return_days_bar_chart.svg"
 output_file_name = "temp/cowpoke/nrr_data_output.csv"
+report_html_file_name = "temp/cowpoke/mating_report.html"
+report_pdf_file_name = "temp/cowpoke/mating_report.pdf"
 
 router = APIRouter()
 
@@ -43,13 +46,18 @@ async def non_return_rate_upload(request: Request):
         input_file_path=input_temp_file_name,
         output_file_path=output_file_name,
         returns_bar_chart_file_path=returns_bar_chart_file_name,
-        cow_submission_graph_file_path=cow_submission_graph_file_name,
+        cow_submission_graph_file_path=cow_submissions_chart_file_name,
     )
     non_return_result["farm_name"] = farm_name
     non_return_result["herd_size"] = herd_size
     non_return_result["utcnow"] = str(datetime.datetime.now(datetime.UTC)).replace(" ", "::")
+
     template_path = os.path.join(template_dir, "non_return_results.html")
-    return templates.TemplateResponse(request={}, name=template_path, context=non_return_result)
+    template_resp = templates.TemplateResponse(request={}, name=template_path, context=non_return_result)
+
+    generate_report_html(template_response=template_resp)
+
+    return template_resp
 
 
 @router.get("/cowpoke/nrr-download")
@@ -57,9 +65,15 @@ async def non_return_rate_download():
     return FileResponse(path=output_file_name, filename="matings_with_returns.csv")
 
 
+@router.get("/cowpoke/mating-report-download")
+async def mating_report_download():
+    generate_report_pdf_from_html()
+    return FileResponse(path=report_pdf_file_name)
+
+
 @router.get("/cowpoke/nrr-images/cow-submissions-chart.svg")
-async def cow_submission_graph_download():
-    return FileResponse(path=cow_submission_graph_file_name)
+async def cow_submissions_chart_download():
+    return FileResponse(path=cow_submissions_chart_file_name)
 
 
 @router.get("/cowpoke/nrr-images/return-days-bar-chart.svg")
@@ -76,10 +90,89 @@ async def non_return_rate_demo(request: Request):
         input_file_path=demo_file_name,
         output_file_path=output_file_name,
         returns_bar_chart_file_path=returns_bar_chart_file_name,
-        cow_submission_graph_file_path=cow_submission_graph_file_name,
+        cow_submission_graph_file_path=cow_submissions_chart_file_name,
     )
     non_return_result["farm_name"] = "Old MacDonald's"
     non_return_result["herd_size"] = 275
     non_return_result["utcnow"] = str(datetime.datetime.now(datetime.UTC)).replace(" ", "::")
+
     template_path = os.path.join(template_dir, "non_return_results.html")
-    return templates.TemplateResponse(request={}, name=template_path, context=non_return_result)
+    template_resp = templates.TemplateResponse(request={}, name=template_path, context=non_return_result)
+
+    generate_report_html(template_response=template_resp)
+
+    return template_resp
+
+
+def generate_report_html(template_response):
+    rendered_html = template_response.body.decode("utf-8")
+
+    report_html = f"""
+<html>
+<head>
+</head>
+<body>
+    {rendered_html}
+</body>
+</html>
+"""
+
+    with open(cow_submissions_chart_file_name) as f:
+        cow_submissions_chart_svg = f.read()
+
+    cow_submssions_chart_figure = f"""
+<figure id="cow_submissions_chart">
+{cow_submissions_chart_svg}
+</figure>
+"""
+    with open(returns_bar_chart_file_name) as f:
+        returns_bar_chart_svg = f.read()
+
+    returns_bar_chart_figure = f"""
+<figure id="return_days_bar_chart">
+{returns_bar_chart_svg}
+</figure>
+"""
+
+    html_with_svg = []
+    replacing_block = False
+
+    for line in report_html.splitlines():
+        if '<figure id="cow_submissions_chart">' in line:
+            replacing_block = True
+            for row in cow_submssions_chart_figure:
+                html_with_svg.append(row)
+        elif '<figure id="return_days_bar_chart">' in line:
+            replacing_block = True
+            for row in returns_bar_chart_figure:
+                html_with_svg.append(row)
+        elif '</figure>' in line:
+            replacing_block = False
+        else:
+            if not replacing_block:
+                html_with_svg.append(line + "\n")
+
+    new_html_lines = []
+    delete_block = False
+
+    for line in html_with_svg:
+        if "delete_div_from_pdf" in line:
+            delete_block = True
+        elif delete_block and "</div>" in line:
+            delete_block = False
+        else:
+            if not delete_block:
+                new_html_lines.append(line)
+
+    with open(report_html_file_name, "w") as g:
+        g.writelines(new_html_lines)
+
+
+def generate_report_pdf_from_html():
+    with open(report_html_file_name) as f:
+        report_html = f.read()
+
+    HTML(string=report_html).write_pdf(
+        target=report_pdf_file_name,
+        stylesheets=[CSS(filename="static/report.css")],
+    )
