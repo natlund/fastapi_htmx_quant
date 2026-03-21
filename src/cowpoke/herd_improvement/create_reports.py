fb7f29f7600991700fb1848226d7b4b5e2033705
Nat Lund <natjlund@gmail.com>
2026-03-21 12:12:21 +0000

    Refactor: Abstract out creation of Report worksheets

--- src/cowpoke/herd_improvement/create_reports.py
+++ src/cowpoke/herd_improvement/create_reports.py
@@ -11,95 +11,7 @@ from xlsxwriter import Workbook
 
 def create_excel_spreadsheet(cow_dict: dict, xlsx_file_path: Path, csv_file_path: Path):
 
-    cow_list = []
-    for cow in cow_dict.values():
-        cow_line_dict = {
-            "cow_no": cow["lactation_data"]["line_number"],
-            "num_lactations": cow["lactation_data"]["lact_num"],
-            "milk_solids": cow["lactation_data"]["milk_solids"],
-            "milk_volume": cow["lactation_data"]["milk"],
-            "sire": cow["lactation_data"]["sire_name"],
-            "merit_score": cow["statistics"]["merit_score"],
-            "group": cow["statistics"]["group"],
-        }
-        cow_list.append(cow_line_dict)
-
-    cow_list_3_8 = [x.copy() for x in cow_list if x["num_lactations"] in (3, 4, 5, 6, 7, 8)]
-
-    cow_list.sort(key=lambda x: x["milk_solids"], reverse=True)
-    for idx, cow in enumerate(cow_list):
-        cow["milk_solids_rank"] = idx + 1
-
-    cow_list.sort(key=lambda x: x["merit_score"], reverse=True)
-    for idx, cow in enumerate(cow_list):
-        cow["merit_score_rank"] = idx + 1
-
-    for cow in cow_list:
-        cow["rank_comparison"] = cow["milk_solids_rank"] - cow["merit_score_rank"]
-
-    cow_list_3_8.sort(key=lambda x: x["milk_solids"], reverse=True)
-    for idx, cow in enumerate(cow_list_3_8):
-        cow["milk_solids_rank"] = idx + 1
-
-    cow_list_3_8.sort(key=lambda x: x["merit_score"], reverse=True)
-    for idx, cow in enumerate(cow_list_3_8):
-        cow["merit_score_rank"] = idx + 1
-
-    for cow in cow_list_3_8:
-        cow["rank_comparison"] = cow["milk_solids_rank"] - cow["merit_score_rank"]
-
-    field_name_lookup = {
-        "cow_no": "Cow No",
-        "num_lactations": "No of Lactations",
-        "milk_solids": "305 M/S",
-        "milk_volume": "305 Vol",
-        "merit_score": "Merit Score",
-        "milk_solids_rank": "Rank M/S",
-        "merit_score_rank": "Rank Merit Score",
-        "rank_comparison": "Rank Comparison",
-        "group": "Group",
-        "sire": "Sire",
-    }
-    column_width_lookup = {
-        "cow_no": 15,
-        "num_lactations": 20,
-        "milk_solids": 15,
-        "milk_volume": 15,
-        "merit_score": 15,
-        "milk_solids_rank": 15,
-        "merit_score_rank": 20,
-        "rank_comparison": 30,
-        "group": 9,
-        "sire": 20,
-    }
-    field_order = (
-        "cow_no", "num_lactations", "milk_solids", "milk_volume", "merit_score",
-        "milk_solids_rank", "merit_score_rank", "rank_comparison", "group", "sire"
-    )
-    rank_comparison_column = field_order.index("rank_comparison")
-
     workbook = Workbook(filename=xlsx_file_path)
-
-    bold_format = workbook.add_format({"bold": True})
-
-    header_format = workbook.add_format()
-    header_format.set_align("center")
-    header_format.set_bold()
-    header_format.set_bg_color("#8faadc")
-    header_format.set_border()
-
-    data_format = workbook.add_format()
-    data_format.set_border()
-
-    centred_data_format = workbook.add_format()
-    centred_data_format.set_align("center")
-    centred_data_format.set_border()
-
-    databars_format = {
-        "type": "data_bar", "bar_solid": True, "bar_color": "green", "bar_negative_color": "red",
-        "bar_axis_position": "middle", "bar_axis_color": "black",
-        # "min_type": "percent", "max_type": "percent", "min_value": "10", "max_value": "90",
-    }
 
     #################################################################################################
     # Data Summary copied from CSV file that has already been constructed.
@@ -152,61 +64,128 @@ def create_excel_spreadsheet(cow_dict: dict, xlsx_file_path: Path, csv_file_path
 
     data_summary.set_column(first_col=0, last_col=32, width=15)
 
-    ################################################################################################
-    # Report for all cows.
+    workbook = create_report_and_report_3_8_worksheets(cow_dict=cow_dict, workbook=workbook)
 
-    report = workbook.add_worksheet("Report")
+    workbook.close()
+
+
+def create_report_and_report_3_8_worksheets(cow_dict: dict, workbook: Workbook) -> Workbook:
+    # Transform data into form useful for Report worksheets.
+
+    cow_list = []
+    for cow in cow_dict.values():
+        cow_line_dict = {
+            "cow_no": cow["lactation_data"]["line_number"],
+            "num_lactations": cow["lactation_data"]["lact_num"],
+            "milk_solids": cow["lactation_data"]["milk_solids"],
+            "milk_volume": cow["lactation_data"]["milk"],
+            "sire": cow["lactation_data"]["sire_name"],
+            "merit_score": cow["statistics"]["merit_score"],
+            "group": cow["statistics"]["group"],
+        }
+        cow_list.append(cow_line_dict)
+
+    workbook = create_report_worksheet(cow_list=cow_list, workbook=workbook, worksheet_name="Report")
+
+    cow_list_3_8 = [x.copy() for x in cow_list if x["num_lactations"] in (3, 4, 5, 6, 7, 8)]
+
+    workbook = create_report_worksheet(cow_list=cow_list_3_8, workbook=workbook, worksheet_name="Report 3-8")
+
+    return workbook
+
+
+def create_report_worksheet(cow_list: list[dict], workbook: Workbook, worksheet_name: str) -> Workbook:
+
+    # Calculate Ranks and Rank Comparison.
+
+    cow_list.sort(key=lambda x: x["milk_solids"], reverse=True)
+    for idx, cow in enumerate(cow_list):
+        cow["milk_solids_rank"] = idx + 1
+
+    cow_list.sort(key=lambda x: x["merit_score"], reverse=True)
+    for idx, cow in enumerate(cow_list):
+        cow["merit_score_rank"] = idx + 1
+
+    for cow in cow_list:
+        cow["rank_comparison"] = cow["milk_solids_rank"] - cow["merit_score_rank"]
+
+    # Define Formats.
+    bold_format = workbook.add_format({"bold": True})
+
+    header_format = workbook.add_format()
+    header_format.set_align("center")
+    header_format.set_bold()
+    header_format.set_bg_color("#8faadc")
+    header_format.set_border()
+
+    data_format = workbook.add_format()
+    data_format.set_border()
+
+    centred_data_format = workbook.add_format()
+    centred_data_format.set_align("center")
+    centred_data_format.set_border()
+
+    databars_format = {
+        "type": "data_bar", "bar_solid": True, "bar_color": "green", "bar_negative_color": "red",
+        "bar_axis_position": "middle", "bar_axis_color": "black",
+        # "min_type": "percent", "max_type": "percent", "min_value": "10", "max_value": "90",
+    }
+
+    field_name_to_column_name = {
+        "cow_no": "Cow No",
+        "num_lactations": "No of Lactations",
+        "milk_solids": "305 M/S",
+        "milk_volume": "305 Vol",
+        "merit_score": "Merit Score",
+        "milk_solids_rank": "Rank M/S",
+        "merit_score_rank": "Rank Merit Score",
+        "rank_comparison": "Rank Comparison",
+        "group": "Group",
+        "sire": "Sire",
+    }
+    running_max_col_widths = {
+        key: len(val) for key, val in field_name_to_column_name.items()
+    }
+    field_order = (
+        "cow_no", "num_lactations", "milk_solids", "milk_volume", "merit_score",
+        "milk_solids_rank", "merit_score_rank", "rank_comparison", "group", "sire"
+    )
+
+    report = workbook.add_worksheet(worksheet_name)
 
     report.write(17, 0, "INDIVIDUAL ANIMAL ANALYSIS", bold_format)
 
     for idx, field in enumerate(field_order):
-        report.write(18, idx, field_name_lookup[field], header_format)
+        report.write(18, idx, field_name_to_column_name[field], header_format)
 
     start_row = 19
     for row, cow in enumerate(cow_list):
-        for col, element in enumerate(field_order):
-            if element in ("rank_comparison", "sire"):
+        for col, field in enumerate(field_order):
+            if field in ("rank_comparison", "sire"):
                 format = data_format
             else:
                 format = centred_data_format
-            report.write(start_row + row, col, cow[element], format)
+            data_value = cow[field]
+            report.write(start_row + row, col, data_value, format)
 
+            if len(str(data_value)) > running_max_col_widths[field]:
+                running_max_col_widths[field] = len(str(data_value))
+
+    # Add databars to rank_comparison column.
     end_row = start_row + len(cow_list)
+    rank_comparison_column = field_order.index("rank_comparison")
     report.conditional_format(
         first_row=start_row, first_col=rank_comparison_column, last_row=end_row, last_col=rank_comparison_column,
         options=databars_format
     )
+
+    # Set column widths for easy viewing.
     for idx, field in enumerate(field_order):
-        report.set_column(first_col=idx, last_col=idx, width=column_width_lookup[field])
+        col_width = running_max_col_widths[field] + 4
+        if col_width >= 9:  # Default column width is ~8.7 units(?)  Don't make column narrower than default.
+            report.set_column(first_col=idx, last_col=idx, width=col_width)
 
-    ##########################################################################################################
-    # Report 3-8 for cows in seasons 3-8.
-
-    report_3_8 = workbook.add_worksheet("Report 3-8")
-
-    report_3_8.write(17, 0, "INDIVIDUAL ANIMAL ANALYSIS", bold_format)
-
-    for idx, field in enumerate(field_order):
-        report_3_8.write(18, idx, field_name_lookup[field], header_format)
-
-    start_row = 19
-    for row, cow in enumerate(cow_list_3_8):
-        for col, element in enumerate(field_order):
-            if element in ("rank_comparison", "sire"):
-                format = data_format
-            else:
-                format = centred_data_format
-            report_3_8.write(start_row + row, col, cow[element], format)
-
-    end_row = start_row + len(cow_list_3_8)
-    report_3_8.conditional_format(
-        first_row=start_row, first_col=rank_comparison_column, last_row=end_row, last_col=rank_comparison_column,
-        options=databars_format
-    )
-    for idx, field in enumerate(field_order):
-        report_3_8.set_column(first_col=idx, last_col=idx, width=column_width_lookup[field])
-
-    workbook.close()
+    return workbook
 
 
 class SlideLayout(Enum):
